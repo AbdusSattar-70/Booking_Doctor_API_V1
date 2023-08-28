3c67f5afd5f1f5e95a33ce875710477dd6f52b31
Abdus Sattar <sasorganicagro@gmail.com>
2023-08-28 15:34:07 +0600

    Modify users general controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[destroy]
-  before_action :authorize_super_admin_or_admin, only: %i[create destroy]
+  before_action :authenticate_user!, only: %i{create destroy}
 
   # GET /users
   def index
     role = params[:role]
 
-    @users = if role == 'doctor'
+    @users = case role
+             when 'doctor'
                User.doctors
-             elsif role == 'patient'
+             when 'patient'
                User.patients
-             elsif role == 'admin'
+             when 'admin'
                User.admin_users
              else
                # If the role is not specified or invalid, fetch all users
@@ -26,17 +26,50 @@ class UsersController < ApplicationController
     render json: @user
   end
 
+  # GET /users/doctors/:id
+  def show_doctor
+    @user = User.doctors.find_by(id: params[:id])
+    if @user.nil?
+      render json: { error: 'Doctor not found' }, status: :not_found
+    else
+      @appointments = @user.appointments
+      render json: { user: @user, appointments: @appointments }
+    end
+  end
+
   # DELETE /users/:id
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    head :no_content
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      render json: { message: 'Not Found', data: { code: 401 } }, status: :not_found
+    else
+        authorize_super_admin_or_admin
+      begin
+        @user.destroy
+        head :no_content
+      rescue ActiveRecord::InvalidForeignKey
+        referenced_records = find_referenced_records(@user)
+        render json: { message: 'Cannot delete the user', references: referenced_records,
+                       data: { code: 402 } }, status: :unprocessable_entity
+      rescue StandardError
+        render json: { message: 'An error occurred while processing the request.', data: { code: 403 } },
+               status: :internal_server_error
+      end
+    end
   end
 
   private
 
+  def find_referenced_records(user)
+    referenced_records = []
+    appointments = user.appointments
+    referenced_records << appointments if appointments.any?
+    referenced_records
+  end
+
   def authorize_super_admin_or_admin
-    return if current_user.super_admin? || current_user.admin?
+     return if current_user.super_admin? || current_user.admin?
 
     render json: { error: 'Unauthorized' }, status: :unauthorized
   end
